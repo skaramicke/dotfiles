80dbe2c6ff58dde74aee827324a0d91a35f81462
Mikael Grön <skaramicke@gmail.com>
2025-02-27 14:59:12 +0100

    Add configuration files for Trunk, Markdownlint, and Shellcheck; update ghostty keybindings and refactor directory automation script

--- .zshrc.d/dir_automation.sh
+++ .zshrc.d/dir_automation.sh
@@ -1,11 +1,9 @@
-#!/bin/zsh
-
 # When entering a directory, load .zshrc.local if it exists
 function chpwd() {
-  if [[ -f .zshrc.local ]]; then
-    source .zshrc.local
-  fi
-  if [[ -f .nvmrc ]]; then
-    nvm use
-  fi
+	if [[ -f .zshrc.local ]]; then
+		source .zshrc.local
+	fi
+	if [[ -f .nvmrc ]]; then
+		nvm use
+	fi
 }
